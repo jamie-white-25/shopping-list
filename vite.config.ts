d47561f7b4abc0032e0c925a831ab03b7de01368
jamie-white-25 <jw4gcc127@gmail.com>
2022-08-04 22:36:47 +0100

    deploy.sh added

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/shopping-list/",
+  base: "/shopping-list/",
   plugins: [vue()],
   resolve: {
     alias: {
